d78d75fcdf1fe962eec58ee363d148209398a467
karinamalgazhdarova <malgazhdarova.karina@gmail.com>
2019-10-15 21:55:07 +0600

    Добавьте файлы проекта.

---
+++ TestBamboo/Logic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestBamboo
+{
+    public class Logic
+    {
+        public Boolean aIsa(Object a)
+        {
+            return a == a && a.Equals(a);
+        }
+
+        public Boolean chainOnEquality(Object a, Object b, Object c)
+        {
+            if (a.Equals(b) && b.Equals(c))
+            {
+                return a.Equals(c);
+            }
+            return false;
+        }
+
+        public Boolean aIsB(Object a, Object b)
+        {
+            return a.Equals(b) == b.Equals(a);
+        }
+    }
+}
